e1223a9b02d2dd5890aa3bfa543c5d8f110a4751
Uy Ha <hchanuy@gmail.com>
2024-03-24 13:37:25 +0000

    finish adding integer literal

--- grammar.js
+++ grammar.js
@@ -1,3 +1,33 @@
+access_specifiers = ["public", "protected", "private"];
+// prettier-ignore
+integer_suffixes = [
+  "u", "l", "ll", "z",
+  "U", "L", "LL", "Z",
+
+  "ul", "ull", "uz",
+  "Ul", "Ull", "Uz",
+
+  "uL", "uLL", "uZ",
+  "UL", "ULL", "UZ",
+
+  "lu", "lU",
+  "llu", "llU",
+  "zu", "zU",
+
+  "Lu", "LU",
+  "LLu", "LLU",
+  "Zu", "ZU",
+];
+// prettier-ignore
+binary_operators = [
+  "*", "/", "%", "+", "-",
+  "<<", ">>",
+  "<=>", "<", ">", "<=", ">=", "==", "!=",
+  "&", "^", "|", "&&", "||",
+];
+directions = ["in", "copy", "inout", "out", "move", "forward"];
+this_specifiers = ["implicit", "virtual", "override", "final"];
+
 module.exports = grammar({
   name: "cpp2",
   rules: {
@@ -10,11 +40,12 @@ module.exports = grammar({
         choice(seq(optional("..."), $._unamed_declaration), $.alias),
       ),
 
-    statement: ($) => choice($.compound_statement, $.declaration),
+    statement: ($) =>
+      choice($.compound_statement, $.declaration, $._expression_statement),
 
     compound_statement: ($) => seq("{", repeat($.statement), "}"),
 
-    access_specifier: ($) => choice("public", "protected", "private"),
+    access_specifier: ($) => choice(...access_specifiers),
     _type_declaration: ($) =>
       seq(
         optional($.meta_functions),
@@ -53,20 +84,42 @@ module.exports = grammar({
       seq($._parameter, repeat(seq(",", $._parameter)), optional(",")),
     _parameter: ($) => choice($.parameter_this, $.parameter),
 
-    this_specifier: ($) => choice("implicit", "virtual", "override", "final"),
+    this_specifier: ($) => choice(...this_specifiers),
     parameter_this: ($) =>
       seq(optional($.this_specifier), optional($.direction), "this"),
 
     parameter: ($) => seq($.identifier, ":", $.type_id),
 
-    direction: ($) => choice("in", "copy", "inout", "out", "move", "forward"),
+    direction: ($) => choice(...directions),
 
-    identifier: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    identifier: ($) =>
+      prec.right(
+        seq($._identifier_start, repeat(choice($._identifier_start, $._digit))),
+      ),
+    _identifier_start: ($) => /[a-zA-Z_]/,
+    _digit: ($) => /[0-9]/,
+
+    _literal: ($) => choice($.integer_literal),
+    integer_literal: ($) =>
+      seq(
+        choice(
+          $._binary_literal,
+          $._octal_literal,
+          $._decimal_literal,
+          $._hexadecimal_literal,
+        ),
+        optional($.integer_suffix),
+      ),
+    _binary_literal: ($) => /0[bB][01]+/,
+    _octal_literal: ($) => /0[0-7]*/,
+    _decimal_literal: ($) => /[1-9][0-9]*/,
+    _hexadecimal_literal: ($) => /0[xX][0-9a-fA-F]+/,
+    integer_suffix: ($) => choice(...integer_suffixes.map(token.immediate)),
 
     type_id: ($) => seq(optional($.type_qualifier), choice($.identifier)),
     type_qualifier: ($) => choice("const", "*"),
 
-    unqualified_id: ($) => choice($.identifier),
+    unqualified_id: ($) => prec.left(choice($.identifier)),
     qualified_id: ($) =>
       seq(
         choice($.nested_namespace_specifier, $.member_name_specifier),
@@ -76,12 +129,21 @@ module.exports = grammar({
       choice("::", seq($.unqualified_id, "::")),
     member_name_specifier: ($) => seq($.unqualified_id, "."),
 
-    requires_clause: ($) => seq("requires", $.expression),
+    requires_clause: ($) => seq("requires", $._expression),
 
-    expression: ($) =>
-      choice($.id_expression, $._unamed_declaration, $.binary_expression),
+    _expression_statement: ($) => prec.right(seq($._expression, optional(";"))),
+    _expression: ($) =>
+      choice(
+        $.id_expression,
+        $._literal,
+        $._unamed_declaration,
+        $.binary_expression,
+      ),
 
-    id_expression: ($) => choice($.qualified_id, $.unqualified_id),
-    binary_expression: ($) => "",
+    id_expression: ($) => prec.left(choice($.qualified_id, $.unqualified_id)),
+    binary_expression: ($) =>
+      prec.left(seq($._expression, $.binary_operator, $._expression)),
+
+    binary_operator: ($) => choice(...binary_operators),
   },
 });
